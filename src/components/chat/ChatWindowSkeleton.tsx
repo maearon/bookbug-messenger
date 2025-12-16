e77feccf08685849e412e8f47a5e8e5eda266a87
maearon <manhng132@gmail.com>
2025-12-16 11:01:31 +0700

    ChatWindowSkeleton

--- src/components/chat/ChatWindowSkeleton.tsx
+++ src/components/chat/ChatWindowSkeleton.tsx
@@ -1,7 +1,13 @@
 // import React from "react";
 
+import { Loader2 } from "lucide-react";
+
 const ChatWindowSkeleton = () => {
-  return <div>ChatWindowSkeleton</div>;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <Loader2 className="size-8 animate-spin text-purple-600" />
+    </div>
+  );
 };
 
 export default ChatWindowSkeleton;
